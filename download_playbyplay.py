c57173ee70c3973c8faf9acc9b51e5b83fce4531
JakobWonisch <jakob.wonisch@gmail.com>
2022-09-10 23:27:11 +0200

    finish playbyplay multithreading

--- download_playbyplay.py
+++ download_playbyplay.py
@@ -1,7 +1,7 @@
 from constants import *
 from tools import *
 import json
-import re
+from concurrent.futures import ThreadPoolExecutor, as_completed
 
 
 # load events
@@ -11,24 +11,51 @@ with open("output/games.json", "r") as outfile:
 # all data will be collected in this array
 data = {}
 
-for seasonI in range(2022, 2011, -1):
-    season = str(seasonI)
-    year = {}
-    for event in games[season]:
-        eventList = {}
-        for game in games[season][event]:
-            print(f"processing game {game} at {event} in {season}")
-            urlPlaybyplay = insertValue(query_playbyplay, "game", game)
-            urlStats = insertValue(query_stats, "game", game)
-            eventList[game] = {
+def downloadJson(game, event, season):
+    # print(f"processing game {game} at {event} in {season}")
+    urlPlaybyplay = insertValue(query_playbyplay, "game", game)
+    urlStats = insertValue(query_stats, "game", game)
+    return {
+                "game": game,
+                "event": event,
+                "season": season,
                 "playbyplay": getJson(urlPlaybyplay),
                 "stats": getJson(urlStats)
             }
-        year[event] = eventList
-        data[season] = year
+
+pool = ThreadPoolExecutor(max_workers=40)
+futures = []
+
+for seasonI in range(2022, 2011, -1):
+    season = str(seasonI)
+    for event in games[season]:
+        for game in games[season][event]:
+            futures.append(pool.submit(downloadJson, game, event, season))
     
+nLoaded = 0
+for future in as_completed(futures):
+    result = future.result()
+
+    season = result["season"]
+    event = result["event"]
+    game = result["game"]
+
+    if season not in data:
+        data[season] = {}
+    if event not in data[season]:
+        data[season][event] = {}
+
+    data[season][event][game] = {
+        "playbyplay": result["playbyplay"],
+        "stats": result["stats"]
+    }
+
+    nLoaded = nLoaded + 1
+    print(f"{nLoaded} loaded {game} at {event} in {season}")
+
+    if nLoaded % 250 == 0:
         # write data to file
         with open("output/playbyplay.json", "w") as outfile:
             json.dump(data, outfile, indent = 4)
 
-    
+pool.shutdown()
